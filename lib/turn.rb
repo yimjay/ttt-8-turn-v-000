ad5b8175e8c594677f10674da9d6dd642902fab2
Ji Yim <gsooyim@gmail.com>
2018-04-03 23:52:40 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -34,8 +34,8 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index)
     move(board, index, "X")
+    display_board(board)
   else 
     turn(board)
   end
-  display_board(board)
 end
